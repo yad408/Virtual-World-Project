7c03c5649a22b5e0d72a88a57cff93ae82538987
yadvirsingh <yadvirsingh98@gmail.com>
2019-08-14 22:58:51 -0700

    fire

--- srcpt1/EventComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public final class EventComparator implements Comparator<Event>
-{
-    public int compare(Event lft, Event rht) {
-        return (int)(lft.time - rht.time);
-    }
-}
